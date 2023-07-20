cbce5a8c374e05f18dcfca279e1aa6d386dfc607
Shane Whitmire <dogunbound5@gmail.com>
2023-07-19 22:11:24 -0500

    Autogenerate folder name as pacrb_w64 or pacrb_l64

--- package.sh
+++ package.sh
@@ -9,24 +9,30 @@ fi
 if [ $1 == "--linux" ];
 then
   mkdir release
-  rm -rf release/linux
-  mkdir release/linux
-  cp -r assets/ release/linux/
-  mkdir release/linux/deps
-  cp -r deps/linux/* release/linux/deps/
-  cp -r pacrb_files release/linux/
-  cp target/release/pacrb release/linux/
-  tar -czvf linux_release.tar.gz release/linux/
+  rm -rf release/pacrb_l64
+  mkdir release/pacrb_l64
+  cp -r assets/ release/pacrb_l64/
+  mkdir release/pacrb_l64/deps
+  cp -r deps/linux/* release/pacrb_l64/deps/
+  cp -r files release/pacrb_l64/
+  cp target/release/pacrb release/pacrb_l64/
+  cd release
+  tar -czvf pacrb_l64_release.tar.gz pacrb_l64/
+  cd ..
+  mv release/pacrb_l64_release.tar.gz .
 fi
 
 if [ $1 == "--windows" ];
 then
   mkdir release
-  rm -rf release/windows
-  mkdir release/windows
-  cp -r assets/ release/windows/
-  cp -r deps/windows/* release/windows/
-  cp -r pacrb_files release/windows/
-  cp target/release/pacrb release/windows/
-  tar -czvf windows_release.tar.gz release/windows/
+  rm -rf release/pacrb_w64
+  mkdir release/pacrb_w64
+  cp -r assets/ release/pacrb_w64/
+  cp -r deps/windows/* release/pacrb_w64/
+  cp -r files release/pacrb_w64/
+  cp target/release/pacrb release/pacrb_w64/
+  cd release
+  tar -czvf windows_release.tar.gz pacrb_w64/
+  cd ..
+  mv release/windows_release.tar.gz .
 fi
